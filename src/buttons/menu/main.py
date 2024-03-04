a4c0b7caf629abb7f8f0d8a6f28386a750553bec
annashutova <annashut@inbox.ru>
2024-03-04 15:00:43 +0300

    added buttons of main  menu

---
+++ src/buttons/menu/main.py
@@ -0,0 +1,13 @@
+from aiogram import types
+
+
+RANDOM_SETUP = 'Рандомно'
+MANUAL_SETUP = 'Вручную'
+
+
+def get_keyboard() -> types.ReplyKeyboardMarkup:
+    kb = [
+        [types.KeyboardButton(text=RANDOM_SETUP)],
+        [types.KeyboardButton(text=MANUAL_SETUP)],
+    ]
+    return types.ReplyKeyboardMarkup(keyboard=kb, one_time_keyboard=True)
