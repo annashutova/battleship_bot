eb93d98b14a1eee8c2189b2186800f36937601c3
Anna Shutova <annashut@inbox.ru>
2024-04-23 10:07:58 +0300

    states

--- src/state/game.py
+++ src/state/game.py
@@ -3,5 +3,4 @@ from aiogram.fsm.state import State, StatesGroup
 
 class GameState(StatesGroup):
     ship_setup = State()
-
     start_game = State()
--- src/state/stats.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class StatsState(StatesGroup):
-    show_stats = State()
