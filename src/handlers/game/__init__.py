39eb4c03f44d9aee02a38fe0d523b4f6fa6e6167
annashutova <annashut@inbox.ru>
2024-05-13 21:51:35 +0300

    small fixes

--- src/handlers/game/__init__.py
+++ src/handlers/game/__init__.py
@@ -1,2 +1,2 @@
 from .game import start_game
-from .ship_setup import manual_setup, random_setup
+from .ship_setup import initial_manual_setup, manual_setup, random_setup
--- src/handlers/game/ship_setup.py
+++ src/handlers/game/ship_setup.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from aiogram import F, types
 from aiogram.fsm.context import FSMContext
 from aiohttp.client_exceptions import ClientResponseError
@@ -32,49 +34,65 @@ async def random_setup(message: types.Message, state: FSMContext) -> None:
     await state.set_state(GameState.start_game)
 
 
-@game_router.message(F.text == MANUAL_SETUP)
+@game_router.message(F.text == MANUAL_SETUP, GameState.ship_setup)
+async def initial_manual_setup(message: types.Message, state: FSMContext) -> None:
+    state_data = await state.get_data()
+
+    try:
+        data = (
+            await do_request(
+                f'{settings.BACKEND_HOST}/game/player_board',
+                method='GET',
+            )
+        )['data']
+    except ClientResponseError:
+        await message.answer('Произошла ошибка, попробуйте еще раз.')
+        return
+
+    await _common_ship_setup(message, state, state_data, data)
+
+
 @game_router.message(GameState.ship_setup)
 async def manual_setup(message: types.Message, state: FSMContext) -> None:
     state_data = await state.get_data()
 
-    if message.text != MANUAL_SETUP:
-        ship_size = state_data['current_ship']
-        try:
-            coords = parse_coordinates(message.text, ship_size)
-        except ValueError:
-            await message.answer(
-                f'Вы ввели неправильные координаты для {ship_size}-палубного корабля.\n' 'Попробуйте снова'
-            )
-            return
+    ship_size = state_data['current_ship']
+    try:
+        coords = parse_coordinates(message.text, ship_size)
+    except ValueError:
+        await message.answer(
+            f'Вы ввели неправильные координаты для {ship_size}-палубного корабля.\n' 'Попробуйте снова'
+        )
+        return
 
-        # отправляем координаты на бэк
-        try:
-            data = (
-                await do_request(
-                    f'{settings.BACKEND_HOST}/game/create_ship',
-                    json_data={'coords': coords},
-                )
-            )['data']
-        except ClientResponseError:
-            await message.answer(
-                f'Вы ввели неправильные координаты для {ship_size}-палубного корабля.\n' 'Попробуйте снова'
+    # отправляем координаты на бэк
+    try:
+        data = (
+            await do_request(
+                f'{settings.BACKEND_HOST}/game/create_ship',
+                json_data={'coords': coords},
             )
-            return
-    else:
-        # если это первый заход в хэнжлер, поле получаем отдельно
-        try:
-            data = (
-                await do_request(
-                    f'{settings.BACKEND_HOST}/game/player_board',
-                    method='GET',
-                )
-            )['data']
-        except ClientResponseError:
-            await message.answer('Произошла ошибка, попробуйте еще раз.')
-            return
+        )['data']
+    except ClientResponseError:
+        await message.answer(
+            f'Вы ввели неправильные координаты для {ship_size}-палубного корабля.\n' 'Попробуйте снова'
+        )
+        return
 
+    await _common_ship_setup(message, state, state_data, data)
+
+
+async def _common_ship_setup(
+    message: types.Message,
+    state: FSMContext,
+    state_data: dict[str, Any],
+    response_data: dict[str, Any],
+) -> None:
     # выводим поле игрока
-    await message.answer('Ваше поле:\n\n' + parse_board(data['player_board']), reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(
+        'Ваше поле:\n\n' + parse_board(response_data['player_board']),
+        reply_markup=types.ReplyKeyboardRemove(),
+    )
 
     ship_types = state_data.get('ship_types')
     if ship_types is None:
@@ -95,8 +113,7 @@ async def manual_setup(message: types.Message, state: FSMContext) -> None:
 
     if ship_types:
         ship = ship_types.pop(0)
-        await state.update_data({'current_ship': ship})
-        await state.update_data({'ship_types': ship_types})
+        await state.update_data({'current_ship': ship, 'ship_types': ship_types})
 
         await message.answer(
             f'Куда вы хотите поставить {ship}-палубный корабль?\n' 'Пример для 2-палубного корабля: C3D3 или c3d3'
